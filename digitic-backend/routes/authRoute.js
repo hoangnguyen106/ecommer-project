d5c7cf367a5dc18d9f5e16409976be16f859c0d9
Hoàng <nguyenhoangnhom4@gmail.com>
2023-09-27 00:29:55 +0700

    fix-be-checkout-27/09

--- digitic-backend/routes/authRoute.js
+++ digitic-backend/routes/authRoute.js
@@ -28,6 +28,8 @@ const {
   updateProductQuantityFromCart,
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const { checkout, paymentVerification } = require("../controller/paymentCtrl");
+
 const router = express.Router();
 router.post("/register", createUser);
 router.post("/forgot-password-token", forgotPasswordToken);
@@ -38,12 +40,15 @@ router.put("/password", authMiddleware, updatePassword);
 router.post("/login", loginUserCtrl);
 router.post("/admin-login", loginAdmin);
 router.post("/cart", authMiddleware, userCart);
-router.post("/cart/applycoupon", authMiddleware, applyCoupon);
-router.post("/cart/cash-order", authMiddleware, createOrder);
+router.post("/order/checkout", authMiddleware, checkout);
+router.post("/order/payment", authMiddleware, paymentVerification);
+
+// router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.post("/cart/create-order", authMiddleware, createOrder);
 router.get("/all-users", getallUser);
-router.get("/get-orders", authMiddleware, getOrders);
-router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
-router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getAllOrders);
+// router.get("/get-orders", authMiddleware, getOrders);
+// router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
+// router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getAllOrders);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
 router.get("/wishlist", authMiddleware, getWishlist);
@@ -60,14 +65,14 @@ router.put(
   authMiddleware,
   updateProductQuantityFromCart
 );
-router.delete("/empty-cart", authMiddleware, emptyCart);
+// router.delete("/empty-cart", authMiddleware, emptyCart);
 router.delete("/:id", deleteaUser);
-router.put(
-  "/order/update-order/:id",
-  authMiddleware,
-  isAdmin,
-  updateOrderStatus
-);
+// router.put(
+//   "/order/update-order/:id",
+//   authMiddleware,
+//   isAdmin,
+//   updateOrderStatus
+// );
 router.put("/edit-user", authMiddleware, updatedUser);
 router.put("/save-address", authMiddleware, saveAddress);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
--- frontend/src/app/modules/console/components/checkout/checkout.component.ts
+++ frontend/src/app/modules/console/components/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { REGEXP_NAME } from 'src/app/modules/shared/constants/regexp';
 
 @Component({
   selector: 'app-checkout',
@@ -7,4 +11,95 @@ import { Component } from '@angular/core';
 })
 export class CheckoutComponent {
   title = 'Checkout';
+
+  productDetail!: any;
+  shippingForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private fb: FormBuilder
+  ) {
+    this.shippingForm = this.fb.group({
+      country: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      firstname: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      lastname: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      address: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      city: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      other: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      state: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+      pincode: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(REGEXP_NAME),
+        ]),
+      ],
+    });
+  }
+  ngOnInit(): void {
+    this.getAllCart();
+  }
+
+  // Lấy tất cả sản phẩm được thêm vào giỏ hàng
+  getAllCart() {
+    this.authService.getCart().subscribe((res) => {
+      this.productDetail = res;
+      console.log(res);
+    });
+  }
+
+  calculateTotalPrice(): number {
+    return this.productDetail.reduce(
+      (total: number, item: any) => total + item.price * item.quantity,
+      0
+    );
+  }
+
+  // Thêm vào địa chỉ shipping
+  registerUser() {
+    
+  }
 }
